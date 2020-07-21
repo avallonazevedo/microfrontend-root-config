d5f79d3d13fe81a90d435bc89056e940428f819b
Avallon Azevedo <avallon.santos@bexsbanco.com.br>
2020-07-21 15:51:47 -0300

    new server

--- server.js
+++ server.js
@@ -9,4 +9,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist/index.html"));
+});
+
 app.listen(PORT, () => console.log(SERVER_RUNNING_MESSAGE));
